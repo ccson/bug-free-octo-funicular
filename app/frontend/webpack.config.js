262757b296d38e307e847fe91f802a2c86d89977
Colin Son <cson@goguardian.com>
2021-06-18 00:43:27 -0700

    Pass In Env Vars Via Env File

---
+++ app/frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: '.env'
+    })
+  ]
+};
+
+module.exports = config;
